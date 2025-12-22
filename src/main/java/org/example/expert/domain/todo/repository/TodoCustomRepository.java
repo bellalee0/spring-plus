4bf334e90550f3b586e1ec6a0e8d2cda3e8d4b64
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-22 12:53:31 +0900

    [Lv.3-1] feat: QueryDSL 검색 기능 구현
    - 요구 사항 : QueryDSL을 사용하여 Todo 검색 기능 만들기(일정 제목, 유저 닉네임, 생성일 범위)
    - 해결 방법 :
      1. Controller : '/todos/search' API 생성 및 쿼리 파라미터로 검색 조건 입력받기
      2. Service : 전달받은 검색 조건과 Pageable 리포지토리에 전달
      3. TodoCustomRepository : findTodoByConditionsPage 메서드 생성
      4. TodoCustomRepositoryImpl
        ① 입력 내용에 대한 null 검사 후, BooleanBuilder에 추가
        ② 검색 조건으로 todo 조회
        ③ 조회된 todo의 title과 해당 todo와 연결된 manager 수, comment 수로 DTO 프로젝션 진행
        ④ Page 객체로 만들어 반환

--- src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepository.java
@@ -1,9 +1,15 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TodoCustomRepository {
 
     Optional<Todo> findByIdWithUser(Long todoId);
+
+    Page<TodoSearchResponse>  findTodoByConditionsPage(String title, String nickname, LocalDateTime startDateTime, LocalDateTime endDateTime, Pageable pageable);
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepositoryImpl.java
@@ -1,12 +1,24 @@
 package org.example.expert.domain.todo.repository;
 
+import static org.example.expert.domain.comment.entity.QComment.comment;
+import static org.example.expert.domain.manager.entity.QManager.manager;
 import static org.example.expert.domain.todo.entity.QTodo.todo;
 import static org.example.expert.domain.user.entity.QUser.user;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 @RequiredArgsConstructor
 public class TodoCustomRepositoryImpl implements TodoCustomRepository {
@@ -22,5 +34,62 @@ public class TodoCustomRepositoryImpl implements TodoCustomRepository {
                         .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Page<TodoSearchResponse> findTodoByConditionsPage(
+        String title, String nickname, LocalDateTime startDateTime, LocalDateTime endDateTime, Pageable pageable
+    ) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (title != null && !title.isBlank()) {
+            builder.and(todo.title.contains(title));
+        }
+
+        if (nickname != null && !nickname.isBlank()) {
+            builder.and(user.nickname.contains(nickname));
+        }
+
+        if (startDateTime != null) {
+            builder.and(todo.createdAt.after(startDateTime));
+        }
+
+        if (endDateTime != null) {
+            builder.and(todo.createdAt.before(endDateTime));
+        }
+
+        List<TodoSearchResponse> responseList = queryFactory
+            .select(Projections.constructor(TodoSearchResponse.class,
+                    todo.title,
+                    ExpressionUtils.as(
+                        JPAExpressions.select(manager.count())
+                            .from(manager)
+                            .where(manager.todo.eq(todo)),
+                        "managers"
+                    ),
+                    ExpressionUtils.as(
+                        JPAExpressions.select(comment.count())
+                            .from(comment)
+                            .where(comment.todo.eq(todo)),
+                        "comments"
+                    )
+                ))
+            .from(todo)
+            .where(builder)
+            .orderBy(todo.createdAt.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long total = queryFactory
+            .select(todo.count())
+            .from(todo)
+            .where(builder)
+            .fetchOne();
+
+        if (total == null) { total = 0L; }
+
+        return new PageImpl<>(responseList, pageable, total);
     }
 }
